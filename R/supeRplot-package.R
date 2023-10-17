34f8ddc3e0b7c72ab17a359c2b43b9ef347f349f
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-17 14:07:58 +0200

    Add stringi package

--- R/supeRplot-package.R
+++ R/supeRplot-package.R
@@ -1,5 +1,5 @@
-#' @import dplyr ggforce ggplot2 ggpubr RColorBrewer rstatix scales stringr
-#' @import tidyr
+#' @import dplyr ggforce ggplot2 ggpubr RColorBrewer rstatix scales stringi
+#' @import stringr tidyr
 #' @importFrom magrittr %>%
 #' @rawNamespace import(stats, except = c(filter, lag))
 #' @keywords internal
