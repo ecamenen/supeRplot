20e2e06e8b6b4c54327c4413b14aa34987c6a56b
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-17 14:07:31 +0200

    Add count_cat

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,35 @@ get_colors <- function() {
         brewer.pal(8, "Set2")[-5]
     )
 }
+
+count_cat <- function(x, parse = FALSE, wrap = 20, collapse = FALSE, label = NULL) {
+  x0 <- as.data.frame(x)
+  if (ncol(x0) > 1) {
+    x <- sapply(colnames(x0), function(i) rep(i, pull(x0, i) %>% unlist() %>% sum(na.rm = TRUE)))
+  }
+  x <- unlist(x) %>%
+    stri_trans_general("latin-ascii") %>%
+    str_replace_all("\n", " ")
+  if (parse) {
+    x <- str_to_title(x)
+  }
+  df <- factor(x) %>%
+    # fct_relabel(~ str_replace_all(.x, "\\s*\\([^\\)]+\\)", "")) %>%
+    fct_relabel(~ str_replace_all(.x, "\\$\\$[^\\)]+", "")) %>%
+    fct_relabel(~ str_replace_all(.x, "^0$", "No")) %>%
+    fct_relabel(~ str_replace_all(.x, "^1$", ifelse(colnames(x0)[1] == "x", "Yes", colnames(x0)[1]))) %>%
+    str_wrap(wrap) %>%
+    fct_infreq() %>%
+    fct_rev() %>%
+    fct_count()
+  if (collapse) {
+    df <- group_by(df, n) %>%
+      summarise(f = paste(f, collapse = ", ") %>% str_wrap(wrap)) %>%
+      mutate(f = factor(f))
+    df$f <- reorder(df$f, df$n)
+  }
+  if (!is.null(label)) {
+    df$f <- factor(df$f, labels = rev(str_wrap(label, wrap)))
+  }
+  return(df)
+}
