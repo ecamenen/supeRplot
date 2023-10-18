92a88756e193ad64b45d026bce1a22599042e187
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-18 13:46:57 +0200

    Modify parameters of plot_bar_mcat and format

--- R/plot_bar_mcat.R
+++ R/plot_bar_mcat.R
@@ -1,59 +1,82 @@
 plot_bar_mcat <- function(
     x,
-    colors = c("blue", "gray", "#cd5b45"),
-    hjust = -0.1,
-    vjust = 0.5,
-    ratio = 5,
-    cex = 10,
+    sample_size = NULL,
     title = NULL,
-    wrap = 20,
-    parse = FALSE,
-    collapse = FALSE,
-    label = NULL,
-    n = Inf,
-    n_collapse = 5,
-    dec = 0,
+    width_text = 20,
+    width_title = width_text,
+    colour = c("blue", "gray", "#cd5b45"),
     color_title = "black",
-    df0 = NULL,
-    wrap_title = wrap,
-    hjust_title = -0.5
-) {
-  if (is.null(title)) {
-    title <- deparse(substitute(x))
-  }
-  x0 <- as.data.frame(x)
-  df <- count_cat(x0, parse = parse, wrap = wrap, collapse = collapse, label = label) %>% data.frame(., order = as.numeric(rownames(.))) %>% tail(n)
-  if (!is.null(df0)) {
-    x0 <- df0
-  }
-  colors <- colorRampPalette(tail(colors, n))(nrow(df))
-  x_lab <- (round(df$n, 2) / nrow(x0) * 100) %>%
-    round(dec) %>%
-    paste0("%")
-  df$x_lab <- x_lab # paste0(df$n, "\n   (", x_lab, ")")
-  df$y_lab <- df$n / 2
-  y_lab0 <- as.character(df$f)
-  y_lab0[(str_count(y_lab0, "\\,") + 1) > n_collapse] <- "..."
-  # i <- df$y_lab < threshold
-  # df$x_lab[i] <- ""
-  (ggplot(df, aes(f, n, fill = order, label = n)) +
-      geom_bar(stat = "identity") +
-      expand_limits(y = max(df$n) + max(df$n) / ratio) +
-      coord_flip() +
-      scale_x_discrete(labels = y_lab0)
-  ) %>% theme_perso_bar(colors = colors, cat = FALSE) +
-    geom_text(
-      aes(color = I("white"), y = y_lab),
-      size = cex
-    ) +
-    geom_text(aes(label = x_lab, color = colors), data = df, hjust = hjust, vjust = vjust, size = cex) +
-    ggtitle(str_wrap(title, wrap_title)) +
-    theme(
-      plot.title = element_text(hjust = hjust_title, vjust = 0, size = cex * 4, face = "bold", color = color_title),
-      axis.text.y = element_text(colour = colors, size = cex * 3),
-      axis.text.x = element_text(size = cex * 2.2),
-      plot.margin = unit(c(-0, 0, 0, 0.5), "cm"),
-      panel.grid.major.y = element_blank()
-    )  +
-    scale_y_continuous(breaks = breaks_pretty(2), labels = label_number_auto())
+    cex = 10,
+    digits = 0,
+    collapse = FALSE,
+    ratio = 5,
+    # label = NULL,
+    n_collapse = 5,
+    n_max = Inf,
+    threshold = 1,
+    hjust_title = -0.5,
+    hjust_text = -0.1,
+    vjust_text = 0.5) {
+    if (is.null(title)) {
+        title <- deparse(substitute(x))
+    }
+    x0 <- as.data.frame(x)
+    df <- count_cat(
+        x0,
+        width = width_text,
+        collapse = collapse
+        # label = label
+    ) %>%
+        data.frame(., order = as.numeric(rownames(.))) %>%
+        tail(n_max)
+    if (is.null(sample_size)) {
+        sample_size <- nrow(x0)
+    }
+    colour <- colorRampPalette(tail(colour, n_max))(nrow(df))
+    x_lab <- (round(df$n, 2) / sample_size * 100) %>%
+        round(digits) %>%
+        paste0("%")
+    df$x_lab <- x_lab
+    df$y_lab <- df$n / 2
+    y_lab0 <- as.character(df$f)
+    y_lab0[(str_count(y_lab0, "\\,") + 1) >= n_collapse] <- "..."
+    i <- df$y_lab < threshold
+    df$x_lab[i] <- ""
+    (ggplot(df, aes(f, n, fill = order, label = n)) +
+        geom_bar(stat = "identity") +
+        expand_limits(y = max(df$n) + max(df$n) / ratio) +
+        coord_flip() +
+        scale_x_discrete(labels = y_lab0)
+    ) %>%
+        theme_bar(colors = colour, cat = FALSE) +
+        geom_text(
+            aes(color = I("white"), y = y_lab),
+            size = cex
+        ) +
+        geom_text(
+            aes(label = x_lab, color = colour),
+            data = df,
+            hjust = hjust_text,
+            vjust = vjust_text,
+            size = cex
+        ) +
+        ggtitle(str_wrap(title, width_title)) +
+        theme(
+            plot.title = element_text(
+                hjust = hjust_title,
+                vjust = 0,
+                size = cex * 4,
+                face = "bold",
+                color = color_title
+            ),
+            axis.text.y = element_text(colour = colour, size = cex * 3),
+            axis.text.x = element_text(size = cex * 2.2),
+            plot.margin = unit(c(-0, 0, 0, 0.5), "cm"),
+            panel.grid.major.y = element_blank()
+        ) %>%
+        suppressWarnings() +
+        scale_y_continuous(
+            breaks = breaks_pretty(2),
+            labels = label_number_auto()
+        )
 }
