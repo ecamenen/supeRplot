1c17c713a48702fa80dbee5d8e762e1c91708e96
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-24 18:52:09 +0200

    Modified parameters for plot_radar

--- R/plot_radar.R
+++ R/plot_radar.R
@@ -1,37 +1,35 @@
 plot_radar <- function(
     x,
     colour = get_colors(),
-    maxmin = FALSE,
-    perc = TRUE,
-    dec = 0,
-    seg = 2,
-    col_alpha = 0.25) {
-  if (!is.null(ncol(x))) {
-      x <- t(df)
-      n <- ncol(x)
-      colors <- colour[seq(n)]
+    cex = 1,
+    n_max = NULL,
+    add_percent = FALSE,
+    digits = 0,
+    n_interval = 2,
+    alpha = 0.25) {
+    if (!is.null(ncol(x))) {
+        x <- t(df)
+        n <- ncol(x)
+        colors <- colour[seq(n)]
     } else {
-      n <- length(x)
-      colors <- colour[1]
+        n <- length(x)
+        colors <- colour[1]
     }
-    if (maxmin) {
-        v_max <- 100
-        caxislabels <- c("50%", "100%")
-    } else {
-        v_max <- unlist(x) %>%
+    if (is.null(n_max)) {
+        n_max <- unlist(x) %>%
             as.numeric() %>%
             max(na.rm = TRUE) %>%
-            round(dec)
-        caxislabels <- seq(0, v_max, length = seg + 1) %>%
-            .[-1] %>%
-            round(dec)
-        if (perc) {
-            caxislabels <- paste0(caxislabels, "%")
-        }
+            round(digits)
+    }
+    caxislabels <- seq(0, n_max, length = n_interval + 1) %>%
+        .[-1] %>%
+        round(digits)
+    if (add_percent) {
+        caxislabels <- paste0(caxislabels, "%")
     }
     x[is.na(x)] <- 0
     x0 <- rbind(
-        rep(v_max, n),
+        rep(n_max, n),
         rep(0, n)
     ) %>%
         set_rownames(c("Max", "Min")) %>%
@@ -41,28 +39,31 @@ plot_radar <- function(
     apply(x0, 2, as.numeric) %>%
         as.data.frame() %>%
         radarchart(
-            cglwd = 2,
-            cglty = 5,
+            cglwd = 3,
+            cglty = 2,
             cglcol = "gray",
             plty = 1,
             pcol = colors,
-            pfcol = alpha(colors, col_alpha),
+            pfcol = alpha(colors, alpha),
             plwd = 4,
             axistype = 1,
-            axislabcol = "black",
+            axislabcol = colors[1],
             caxislabels = c("", caxislabels),
-            seg = seg,
-            vlcex = 1.5,
-            calcex = 1.25,
+            seg = n_interval,
+            vlcex = cex * 1.5,
+            calcex = cex * 1.25,
             centerzero = TRUE
         )
-    legend(
-        "bottomleft",
-        legend = rownames(x),
-        bty = "n",
-        pch = 20,
-        col = colors,
-        text.col = "black",
-        pt.cex = 2
-    )
+    if (!is.null(ncol(x))) {
+        legend(
+            "bottomleft",
+            legend = rownames(x),
+            bty = "n",
+            pch = 15,
+            col = colors,
+            text.col = "black",
+            pt.cex = cex * 2.5,
+            cex = cex * 1.5
+        )
+    }
 }
