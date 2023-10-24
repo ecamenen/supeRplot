ed1bf87e086357b6cc150d80bb6ffc0683856e07
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-24 13:59:11 +0200

    Add utils for plot_venn

--- R/utils.R
+++ R/utils.R
@@ -51,3 +51,42 @@ count_cat <- function(x, width = 20, collapse = FALSE) {
 to_title <- function(x) {
     paste0(toupper(substr(x, 1, 1)), substr(x, 2, nchar(x)))
 }
+
+
+# @export
+# Cuts a sentence to a given number of characters and leaves the words as integers
+# @example str_trunc1("Hi there, I'm a sentence to format.")
+str_trunc1 <- function(x, n = 20, w = " ") {
+    x0 <- strsplit(x, w)[[1]]
+    lapply(seq(length(x0)), function(i) str_trunc0(x, i, w)) %>%
+        detect(function(x) str_length(x) <= n, .dir = "backward")
+}
+
+# Cuts a sentence to a given number of words
+# @example str_trunc0("Hi there, I'm a sentence to format.")
+str_trunc0 <- function(x, n = 5, w = " ") {
+    strsplit(x, w)[[1]] %>%
+        .[seq(n)] %>%
+        paste(collapse = w)
+}
+
+str_trunc2 <- function(x, n = 20) {
+    sapply(
+        x,
+        function(i) {
+            i <- str_trim(i)
+            res <- str_trunc1(i, n)
+            if (is.null(res)) {
+                res <- str_trunc1(i, n, "-")
+            }
+            if (is.null(res)) {
+                return(res)
+            }
+            if (str_width(res) < str_width(i)) {
+                paste0(res, "...")
+            } else {
+                res
+            }
+        }
+    ) %>% unname()
+}
