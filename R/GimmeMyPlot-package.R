938db0aca7c6a8b8b8b22bc116e5eb7b65f3e36e
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-24 15:51:33 +0200

    Update packages for plot_venn

--- R/GimmeMyPlot-package.R
+++ R/GimmeMyPlot-package.R
@@ -1,8 +1,9 @@
-#' @import dplyr forcats ggforce ggplot2 ggpubr RColorBrewer rstatix scales
-#' @import stringi stringr tidyr
+#' @import dplyr forcats ggforce ggplot2 ggpubr ggVennDiagram purrr RColorBrewer
+#' @import rlist rstatix stringi stringr tidyr
 #' @importFrom grDevices colorRampPalette
 #' @importFrom utils tail head
-#' @rawNamespace import(magrittr, except = extract)
+#' @rawNamespace import(magrittr, except = c(extract, set_names))
+#' @rawNamespace import(scales, except = discard)
 #' @rawNamespace import(stats, except = c(filter, lag))
 #' @keywords internal
 "_PACKAGE"
