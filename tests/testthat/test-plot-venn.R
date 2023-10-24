5de10adf32e03f7921ffd38e518127cb5639e47a
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-24 15:48:46 +0200

    Add vignette and test for plot_venn

---
+++ tests/testthat/test-plot-venn.R
@@ -0,0 +1,34 @@
+x <- lapply(
+    seq(3),
+    function(x) {
+        runif(10, 1, 10) %>%
+            round() %>%
+            letters[.] %>%
+            unique() %>%
+            paste("Element", .)
+    }
+) %>%
+    set_names(paste("Dataset", LETTERS[seq(3)]))
+
+test_that("venn default works", {
+    expect_s3_class(plot_venn_diagram(x), "ggplot")
+})
+
+test_that("venn advanced works", {
+    p <- plot_venn_diagram(
+        x,
+        width_text = 8,
+        width_label = 5,
+        colour = brewer.pal(3, "Reds"),
+        color_gradient = c("white", "red"),
+        cex = 1.2,
+        cex_main = 1.2 * 6,
+        n_max = 2,
+        vjust_label = 0.75,
+        ratio = 0.5,
+        label = FALSE,
+        element = FALSE,
+        percent = FALSE
+    )
+    expect_s3_class(p, "ggplot")
+})
