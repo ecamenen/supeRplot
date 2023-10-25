a98f3db48bd573b1b3770b5f37b6c21d74e8f44d
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-26 00:07:36 +0200

    Add test and vignette for plot_alluvial

---
+++ tests/testthat/test-plot-alluvial.R
@@ -0,0 +1,25 @@
+library(ggalluvial)
+
+x <- lapply(seq(3), function(x) {
+    runif(100, 1, 3) %>%
+        round() %>%
+        letters[.]
+}) %>%
+    as.data.frame() %>%
+    set_colnames(paste("variable", LETTERS[seq(3)]))
+x[x == "a"] <- NA
+x[, 3][is.na(x[, 3])] <- "a"
+
+test_that("piechart default works", {
+    expect_s3_class(plot_alluvial(x), "ggplot")
+})
+
+test_that("piechart advanced works", {
+    p <- plot_alluvial(
+        x,
+        width_label = 5,
+        colour = rev(brewer.pal(3, "Reds")),
+        cex = 1.5
+    )
+    expect_s3_class(p, "ggplot")
+})
