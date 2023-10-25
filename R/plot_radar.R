c795ea559f7c06e2e41505c75270bbc3bb6b497d
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-25 11:59:01 +0200

    Add doc for plot_radar

--- R/plot_radar.R
+++ R/plot_radar.R
@@ -1,3 +1,49 @@
+#' Radar plot
+#'
+#' Display multivariate data in the form of a two-dimensional chart of three
+#' or more quantitative variables represented on axes starting from the same
+#' point. The relative position and angle of the axes is typically
+#' uninformative. The radar chart is also known as web chart, spider chart,
+#' star chart, cobweb chart, irregular polygon, polar chart, or kiviat diagram.
+#'
+#' @param x either a integer vector (with named elements) or a data frame
+#' containing variables in columns and samples in rows (with named columns
+#'  and rows).
+#' @param colour Color or vector of colors for the samples.
+#' @param cex Double for the size of the labels.
+#' @param n_max Integer for the maximum value of the axis.
+#' @param add_percent Boolean to include percentage values in the axis labels.
+#' @param digits Integer for the number of digits to be displayed.
+#' @param n_interval Integer for the number of intervals for the axis.
+#' @param alpha Double for the transparency.
+#'
+#' @examples
+#' library(magrittr)
+#'
+#' df <- sapply(
+#'     seq(2),
+#'     function(x) {
+#'         runif(10, 1, 100) %>%
+#'             set_names(paste("Variable", letters[seq(10)]))
+#'     }
+#' ) %>% set_colnames(paste("Sample", LETTERS[seq(2)]))
+#'
+#' # Default parameters
+#' plot_radar(df[, 1])
+#'
+#' # Advanced parameters
+#' plot_radar(
+#'     df,
+#'     colour = c("orange", "forestgreen"),
+#'     cex = 0.9,
+#'     n_max = 100,
+#'     digits = 1,
+#'     n_interval = 4,
+#'     alpha = 0.5
+#' )
+#'
+#' @return NULL (launch a basic plot)
+#' @export
 plot_radar <- function(
     x,
     colour = get_colors(),
@@ -8,7 +54,7 @@ plot_radar <- function(
     n_interval = 2,
     alpha = 0.25) {
     if (!is.null(ncol(x))) {
-        x <- t(df)
+        x <- t(x)
         n <- ncol(x)
         colors <- colour[seq(n)]
     } else {
