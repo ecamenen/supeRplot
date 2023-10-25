e15c4125a51371246ea6f99844a0b8e2ce005b3e
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-25 16:54:23 +0200

    Fix plot_alluvial function for multiple variables

--- R/plot_alluvial.R
+++ R/plot_alluvial.R
@@ -1,45 +1,61 @@
 plot_alluvial <- function(
-    df,
+    x,
     col_stratum = NULL,
     col_alluvium = NULL,
     label_stratum = NULL,
     label = NULL) {
-    if (is.null(col_stratum)) {
-        col_stratum <- c(rep(get_colors()[seq(2)], 2))
-    }
-    if (is.null(col_alluvium)) {
-        col_alluvium <- c(rep(c(get_colors()[c(2, 4, 8, 1)], "gray40"), 2))
-    }
-    if (is.null(label)) {
-        label <- str_to_title(colnames(df)[seq(2)])
-    }
-    df <- count(df, pull(df, 1), pull(df, 2))
-    fct_count(pull(df, 1))
-    ggplot(df, aes(y = n, axis1 = pull(df, 1), axis2 = pull(df, 2))) +
-        geom_alluvium(fill = col_alluvium, width = 0.5, knot.pos = 0) +
-        geom_stratum(width = 0.5, fill = col_stratum, color = "grey") +
+    df <- sapply(x, function(x) as.character(x)) %>% as.data.frame()
+    n <- unlist(df) %>%
+        unique() %>%
+        na.omit() %>%
+        seq()
+    df[is.na(df)] <- "zz"
+    df0 <- df %>%
+        mutate(id = rownames(.)) %>%
+        pivot_longer(-id)
+
+    label_stratum <- df %>%
+        select(sort(colnames(.))) %>%
+        lapply(
+            function(i) {
+                unique(i) %>%
+                    sort(decreasing = TRUE) %>%
+                    str_replace_all("zz", "NA")
+            }
+        ) %>%
+        unlist()
+    ggplot(
+        df0,
+        aes(
+            x = name,
+            stratum = value,
+            alluvium = id,
+            fill = value,
+            label = value
+        )
+    ) +
+        geom_flow() +
+        geom_stratum(width = 0.5, color = "grey") +
         geom_text(
             stat = "stratum",
             color = "white",
-            aes(label = after_stat(stratum)),
+            label = label_stratum,
             size = 8
         ) +
-        scale_x_discrete(
-            limits = label,
-            expand = c(.05, .05)
-        ) +
-        scale_fill_manual(values = get_colors()[seq(2)]) +
-        scale_y_continuous(
-            breaks = seq(0, 40, 5),
-            minor_breaks = seq(36),
-            sec.axis = sec_axis(trans = ~., name = "n", breaks = seq(0, 40, 5))
-        ) +
+        scale_x_discrete(expand = c(.05, .05)) +
+        scale_fill_manual(values = c(get_colors()[n], "gray")) +
+        # scale_y_continuous(
+        #     breaks = seq(0, 40, 5),
+        #     minor_breaks = seq(36),
+        #     sec.axis = sec_axis(trans = ~., name = "n", breaks = seq(0, 40, 5))
+        # ) +
         labs(y = "n") +
-        theme_minimal() +
-        theme(
-            legend.position = "none",
-            axis.ticks.x = element_blank(),
-            panel.grid.major.x = element_blank()
-        ) %>%
-        theme_perso0(1.5, show_axis = FALSE)
+        theme_minimal() %>%
+        theme_custom(cex = 1.5, show_axis = FALSE) +
+      theme(
+        legend.position = "none",
+        axis.ticks.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        axis.title.x = element_blank()
+      )
 }
