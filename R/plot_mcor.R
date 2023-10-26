1d6d1495e278aa5b9309ad1844b731666a47954d
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-26 17:24:09 +0200

    Add doc for plot_cor

--- R/plot_mcor.R
+++ R/plot_mcor.R
@@ -1,3 +1,39 @@
+#' Correlation matrix
+#'
+#' Display a heatmap of correlation coefficients (with p-values between several
+#'  numerical variables). Each cell indicates the correlation between two
+#'  variables. The darker the color, the stronger the correlation. Blue
+#'  indicates a positive correlation, red a negative one. Non-significant
+#'  adjusted p-values are crossed out. This is an wrapper for the function
+#'  [corrplot::corrplot()].
+#'
+#' @inheritParams plot_violin
+#' @inheritParams plot_bar
+#' @param x Data.frame of double variables (with column names).
+#' @param colour Color or vector of colors for the gradient of the bars.
+#' @param method Character for the test method ('pearson' or 'spearman').
+#'
+#' @return NULL (launch a basic plot)
+#' @export
+#'
+#' @examples
+#' library(magrittr)
+#' library(RColorBrewer)
+#'
+#' x <- sapply(seq(20), function(x) runif(20)) %>%
+#'     set_colnames(paste("Variable", seq(20)))
+#'
+#' # Default parameters
+#' plot_mcor(x)
+#'
+#' # Advanced parameters
+#' plot_mcor(
+#'     x,
+#'     colour = c("black", brewer.pal(n = 6, name = "RdBu"), 1),
+#'     method = "spearman",
+#'     method_adjust = "none",
+#'     cex = 0.8
+#' )
 plot_mcor <- function(
     x,
     colour = brewer.pal(n = 8, name = "RdBu"),
