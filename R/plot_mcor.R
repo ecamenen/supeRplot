dcdad8fc5d21ba9bc5171808009df7444c1f0846
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-26 15:08:06 +0200

    Add plot_mcor

---
+++ R/plot_mcor.R
@@ -0,0 +1,51 @@
+plot_mcor <- function(
+    x,
+    y = NULL,
+    clean_name = TRUE,
+    mat = NULL,
+    p_mat = NULL,
+    col = brewer.pal(n = 9, name = "RdBu"),
+    method = "spearman",
+    p_adjust = "BH",
+    cex = 1
+) {
+  if (clean_name) {
+    colnames(x) <- get_var_names(colnames(x), y)
+    x <- clean_names(x)
+  }
+  x <- as.data.frame(x)
+  if (is.null(mat)) {
+    mat <- get_corr(x, TRUE, method = method)
+    colnames(mat) <- colnames(x) -> rownames(mat)
+  }
+  if (is.null(p_mat)) {
+    p_mat <- get_corr(x, FALSE, method = method) %>% as.vector() %>% p.adjust(p_adjust) %>% matrix(nrow = sqrt(length(.)), ncol = sqrt(length(.)))
+  }
+
+  corrplot(
+    mat,
+    # method="color",
+    col = col,
+    type = "upper",
+    order = "original",
+    # addCoef.col = "black",
+    # Ajout du coefficient de corrélation
+    tl.col = "gray50",
+    tl.srt = 45,
+    tl.cex = 1 * cex,
+    # Rotation des etiquettes de textes
+    # Combiner avec le niveau de significativité
+    p.mat = p_mat,
+    sig.level = 0.05,
+    addgrid.col = NA,
+    insig = "pch",
+    pch = 4,
+    pch.cex = 2.5 * cex,
+    pch.col = "white",
+    # Cacher les coefficients de corrélation sur la diagonale
+    diag = FALSE,
+    na.label = " ",
+    cl.cex = cex,
+    cl.col = "gray50"
+  )
+}
