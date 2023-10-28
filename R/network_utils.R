3bc074d3783fbcab5ec0e291694dcc4ab15024a5
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-28 18:03:23 +0200

    Rename parameters in network_utils
    * Add labels and digits in get_edges
    * Remove title and color_node in plot_cor_network
    * Rename plot_network2 in plot_network_dyn
    * Remove C in plot_network*
    * Set cex parameter in dynamic for get_nodes

--- R/network_utils.R
+++ R/network_utils.R
@@ -1,12 +1,13 @@
 # Creates the nodes for a design matrix
 get_nodes <- function(x, ratio = 12) {
+get_nodes <- function(x, cex = 12) {
     res <- unlist(x[, seq(2)]) %>%
         data.frame(id = .) %>%
         group_by(id) %>%
         summarise(size = n()) %>%
         as.data.frame()
     n <- pull(res, size) %>% max()
-    res$size <- res$size / n * ratio
+    res$size <- res$size / n * cex
     return(res)
 }
 
@@ -15,18 +16,24 @@ get_nodes <- function(x, ratio = 12) {
 # @return A dataframe with tuples of connected x
 get_edges <- function(x, C, p = NULL) {
     J <- NCOL(C)
+get_edges <- function(x, y = NULL, digits = 2) {
+    stopifnot(isSymmetric(x))
+    if (!is.null(y)) {
+        stopifnot(isSymmetric(y))
+    }
+    n <- NCOL(x)
     edges <- list()
 
     k <- 0
-    for (j in seq(J)) {
-        for (i in seq(J)) {
-            if (i > k && abs(C[i, j]) > 0) {
-                if (is.null(p)) {
+    for (j in seq(n)) {
+        for (i in seq(n)) {
+            if (i > k && abs(x[i, j]) > 0) {
+                if (is.null(y)) {
                     d <- NULL
                 } else {
-                    d <- p[i, j]
+                    d <- y[i, j]
                 }
-                edges[[length(edges) + 1]] <- c(colnames(x)[j], colnames(x)[i], C[i, j], d)
+                edges[[length(edges) + 1]] <- c(colnames(x)[j], colnames(x)[i], x[i, j], d)
             }
         }
         k <- k + 1
@@ -36,10 +43,10 @@ get_edges <- function(x, C, p = NULL) {
     edges <- as.data.frame(t(matrix(unlist(edges), n, length(edges))))
     colnames(edges) <- c("from", "to", "weight", "p")[seq(n)]
     edges[, 3] <- as.numeric(edges[, 3])
-    if (!is.null(p)) {
+    if (!is.null(y)) {
         edges[, 4] <- as.numeric(edges[, 4])
     }
-
+    edges$title <- edges$label <- round(edges[, 3], digits)
     return(edges)
 }
 
@@ -48,26 +55,26 @@ get_edges <- function(x, C, p = NULL) {
 #' @return A dataframe with tuples of connected blocks
 #' @export
 plot_network <- function(
-    x,
-    C = 1 - diag(length(x)),
+    x = NULL,
     title = "",
     cex = 1,
     cex_main = 14 * cex,
-    cex_point = 3 * cex,
-    cex_nodes = 2 * cex,
+    cex_node = 3 * cex,
+    cex_edge = 2 * cex,
     color = c("#eee685", "#686868"),
     shape = "dot",
-    dashes = TRUE,
+    dashed = TRUE,
     nodes = NULL,
     edges = NULL,
     dist = 1,
-    label = FALSE) {
+    label = FALSE,
+    digits = 2) {
     title <- paste0(title, collapse = " ")
-    if (is.null(nodes)) {
-        nodes <- get_nodes(x)
-    }
     if (is.null(edges)) {
-        edges <- get_edges(x, C)
+        edges <- get_edges(x, digits = digits)
+    }
+    if (is.null(nodes)) {
+        nodes <- get_nodes(edges, cex * 12)
     }
     if (!label) {
         edges$label <- ""
@@ -89,12 +96,12 @@ plot_network <- function(
     } else {
         edge_color <- edges$color
     }
-    E(net)$width <- E(net)$weight * cex_nodes
+    E(net)$width <- E(net)$weight * cex_edge
 
     plot(
         net,
         edge.color = edge_color,
-        edge.lty = ifelse(dashes, 2, 1),
+        edge.lty = ifelse(dashed, 2, 1),
         edge.label.color = color[2],
         edge.label.family = "sans",
         edge.label.cex = cex * 0.95,
@@ -104,8 +111,8 @@ plot_network <- function(
         vertex.label.dist = dist,
         vertex.label.degree = 1.5,
         vertex.label.family = "sans",
-        vertex.size = cex_point * nodes$size * 0.5,
-        vertex.frame.width = cex_point * 0.9,
+        vertex.size = cex_node * nodes$size * 0.5,
+        vertex.frame.width = cex_node * 0.9,
         margin = c(0.1, 0, 0, 0)
     )
     title(title, cex.main = cex_main * 0.1)
@@ -118,30 +125,32 @@ plot_network <- function(
 plot_network2 <- function(
     x,
     C = 1 - diag(length(x)),
+plot_network_dyn <- function(
+    x = NULL,
     title = "",
     cex = 1,
     cex_main = 14 * cex,
-    cex_point = 3 * cex,
-    cex_nodes = 2 * cex,
+    cex_node = 3 * cex,
+    cex_edge = 2 * cex,
     color = c("#eee685", "#686868"),
     shape = "dot",
-    dashes = TRUE,
+    dashed = TRUE,
     nodes = NULL,
     edges = NULL) {
     title <- paste0(title, collapse = " ")
     if (length(color) < 2) {
         color <- c(color, "gray")
     }
+    if (is.null(edges)) {
+        edges <- get_edges(x)
+    }
+    edges$width <- edges$weight * cex_edge
     if (is.null(nodes)) {
-        nodes <- get_nodes(x)
+        nodes <- get_nodes(edges, cex * 12)
         nodes$label <- nodes$id
     }
-    nodes$title <- nodes$id -> nodes$label
+    nodes$title <- nodes$label <- nodes$id
     nodes$color.background <- rep(as.vector(color[1]), nrow(nodes))
-    if (is.null(edges)) {
-        edges <- get_edges(x, C)
-    }
-    edges$width <- edges$weight * cex_nodes
 
     visNetwork(
         nodes,
@@ -159,7 +168,7 @@ plot_network2 <- function(
             borderWidth = 2,
             shape = shape,
             shadow = TRUE,
-            size = cex_point * 11,
+            size = cex_node * 11,
             font = list(size = cex * 21, color = color[2]),
             color = list(
                 border = "#686868",
@@ -169,7 +178,7 @@ plot_network2 <- function(
         visEdges(
             smooth = FALSE,
             shadow = TRUE,
-            dashes = dashes,
+            dashes = dashed,
             color = list(color = color[2], highlight = "darkred")
         )
 }
@@ -179,16 +188,17 @@ get_corr1 <- function(
     method = "spearman",
     method_adjust = "BH",
     cutoff = 0.75) {
-    C <- get_corr(x, TRUE, method)
-    C[abs(C) < cutoff] <- 0 -> diag(C)
-    C[is.na(C)] <- 0
+    r <- get_corr(x, TRUE, method)
+    r[abs(r) < cutoff] <- diag(r) <- 0
+    r[is.na(r)] <- 0
     p <- get_corr(x, FALSE, method, method_adjust)
-    C[p >= 0.05] <- 0
-    return(list(C = C, p = p))
+    r[p >= 0.05] <- 0
+    return(list(r = r, p = p))
 }
 
 #' @export
 plot_corr_network <- function(
+plot_cor_network <- function(
     x,
     colour_edge = c("#4DAF4A", "#EE6363"),
     colour_node = c("white", "#3D3D3D"),
@@ -199,26 +209,24 @@ plot_corr_network <- function(
     digits = 2,
     ...) {
     res <- get_corr1(x, method, method_adjust, cutoff)
-    title <- round(mean(res$C, na.rm = TRUE), digits)
-    edges <- get_edges(x, res$C, res$p)
+    edges <- get_edges(res$r, res$p, digits = digits)
     font <- "14px arial black"
     edges$font.bold.mod <- ifelse(edges$p < 0.05, paste(font, "bold"), font)
-    edges$title <- edges$label <- round(edges[, 3], digits)
-    nodes <- get_nodes(edges, cex = 12 * cex)
-    color_node <- edges$color <- ifelse(
+    nodes <- get_nodes(edges, cex = cex * 12)
+    edges$color <- ifelse(
         edges$weight > 0,
         colour_edge[1],
         colour_edge[2]
     )
 
-    plot_network2(
+    plot_network_dyn(
         x,
-        res$C,
-        dashes = FALSE,
+        res$r,
+        dashed = FALSE,
         nodes = nodes,
         edges = edges,
         cex = cex,
-        cex_nodes = edges$weight * 20 * cex,
+        cex_edge = edges$weight * 20 * cex,
         color = c(colour_node[1], colour_node[2]),
         ...
     )
