170f3d3c5082745a4f815e19ef7c562f9b67eb18
Etienne Camenen <etienne.camenen@gmail.com>
2023-10-28 20:39:39 +0200

    Add packages for network_utils

--- R/GimmeMyPlot-package.R
+++ R/GimmeMyPlot-package.R
@@ -1,7 +1,9 @@
-#' @import dplyr fmsb forcats ggalluvial corrplot ggforce ggplot2 ggpubr
-#' @import ggVennDiagram purrr RColorBrewer rlist rstatix stringi stringr tidyr
+#' @import corrplot dplyr fmsb forcats ggalluvial ggforce ggplot2 ggpubr
+#' @import ggVennDiagram purrr RColorBrewer rlist rstatix stringi
+#' @import stringr tidyr visNetwork
 #' @importFrom grDevices colorRampPalette
 #' @importFrom graphics legend
+#' @importFrom igraph E E<- graph_from_data_frame plot.igraph V V<-
 #' @importFrom utils tail head
 #' @rawNamespace import(magrittr, except = c(extract, set_names))
 #' @rawNamespace import(scales, except = discard)
