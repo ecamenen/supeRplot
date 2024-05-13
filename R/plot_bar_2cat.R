be2d8e32f187669c0a1c17b1c1f921f21bb4f0e5
Etienne Camenen <etienne.camenen@gmail.com>
2024-05-13 15:29:28 +0200

    Update plot_bar_2cat
    * Add documentation
    * Modify parameter names
    * Remove colour2

--- R/plot_bar_2cat.R
+++ R/plot_bar_2cat.R
@@ -1,59 +1,61 @@
-# x <- sapply(
-#     list(letters, LETTERS),
-#     function(x) runif(10, 1, 4) %>% x[.]
-# )
-# plot_bar_2cat(x)
-#
-# x <- cbind(
-#   mapply(function(x, y) rep(x, y), letters[seq(3)], c(7, 5, 8)) %>% unlist(),
-#   mapply(function(x, y) rep(x, y), LETTERS[seq(3)], c(6, 6, 8)) %>% unlist()
-# )
-
-# V1 V2
-# 1   a  B
-# 2   c  C
-# 3   b  A
-# 4   c  B
-# 5   b  B
-# 6   a  C
-# 7   a  C
-# 8   a  C
-# 9   a  C
-# 10  c  B
-# 11  b  A
-# 12  a  C
-# 13  c  A
-# 14  b  A
-# 15  a  C
-# 16  b  A
-# 17  c  C
-# 18  a  B
-# 19  c  B
-# 20  a  B
-# file_path <- "http://www.sthda.com/sthda/RDoc/data/housetasks.txt"
-# housetasks <- read.delim(file_path, row.names = 1)
-# sub_housetasks <- housetasks[c(1:2, 12:13), c("Wife", "Husband")]
-# plot_bar_2cat(t(sub_housetasks), count = TRUE, workspace = 1e6)
+#' Plot for two categorical variables
+#'
+#' Visualize the distribution of two categorical variables using a stacked
+#' barplot.
+#'
+#' @inherit plot_violin
+#' @inherit plot_bar_mcat
+#' @param method Character indicating the type of test to be performed,
+#' choosing between "chisq" for the chi-square test or "fisher" for
+#' Fisher's exact test.
+#' @param width_text Integer for the maximum length of the text.
+#' @param width_legend Integer for the maximum length of the legend.
+#' @param ratio Double for scaling the Y-axis.
+#'
+#' @examples
+#' library(magrittr)
+#'
+#' # Default parameters
+#' x <- cbind(
+#'   mapply(function(x, y) rep(x, y), letters[seq(3)], c(7, 5, 8)) %>% unlist(),
+#'   mapply(function(x, y) rep(x, y), LETTERS[seq(3)], c(6, 6, 8)) %>% unlist()
+#' )
+#' plot_bar_2cat(x)
+#' plot_bar_2cat(x, legend = c("c", "b", "a"))
+#'
+#' # Advanced parameters
+#' file_path <- "http://www.sthda.com/sthda/RDoc/data/housetasks.txt"
+#' housetasks <- read.delim(file_path, row.names = 1)
+#' sub_housetasks <- housetasks[c(1:2, 12:13), c("Wife", "Husband")]
+#' plot_bar_2cat(
+#'     t(sub_housetasks),
+#'     colour = palette_discrete()[3:4],
+#'     sort = TRUE,
+#'     threshold = 10,
+#'     count = TRUE,
+#'     workspace = 1e6
+#' )
+#'
+#' @return A ggplot object.
 #' @export
 plot_bar_2cat <- function(
         x,
-        colour1 = NULL,
-        colour2 = "gray50",
-        colour3 = "white",
+        title = NULL,
+        width_text = 30,
+        width_title = 10,
+        width_legend = 10,
+        colour = NULL,
+        colour_text = "white",
         cex = 1,
         cex_main = 21 * cex,
         cex_sub = 15 * cex,
         cex_axis = 21 * cex,
         method = "fisher",
         method_adjust = "BH",
-        width_title = 10,
-        width_legend = 10,
+        sort = TRUE,
+        threshold = 0,
         ratio = 7,
         stats = TRUE,
-        title = NULL,
-        sort = FALSE,
-        width_text = 30,
-        threshold = 0,
         legend = NULL,
         count = FALSE,
         ...
@@ -88,8 +90,8 @@ plot_bar_2cat <- function(
         stopifnot(legend %in% unique(df0$var1) %>% all())
         df0$var1 <- df0$var1 %>% factor(levels = legend)
     }
-    if (is.null(colour1)) {
-        colour1 <- palette_discrete()[seq(unique(pull(x, 1)))]
+    if (is.null(colour)) {
+        colour <- palette_discrete()[seq(unique(pull(x, 1)))]
     }
     counts <- data.frame(table(df0, useNA = "ifany")) %>%
         mutate(label = ifelse(Freq > threshold, Freq, ""))
@@ -105,39 +107,38 @@ plot_bar_2cat <- function(
             method_adjust = method_adjust,
             ...
         ) %>%
-            filter(FDR <= 0.05) %>%
+            filter(p <= 0.05) %>%
             mutate(
                 var1 = df0$var1[1],
                 y.position = max_val +
                     (as.numeric(rownames(.)) * max_val / ratio)
             )
     }
-    # if (isFALSE(sort)) {
-    #   counts$var2 <- counts$var2 %>% factor(levels = unique(.))
-    # }
     p <- ggplot(data = counts, aes(x = var2, y = Freq, fill = var1)) +
         geom_bar(stat = "identity", position = "stack") +
         xlab(to_title(colnames(x)[2])) +
         ylab("Count") +
         scale_fill_manual(
-            values = colour1,
+            values = colour,
             name = to_title(colnames(x)[1]) %>% str_wrap(width_legend),
             na.value = "gray"
         ) +
         geom_text(
             aes(label = label, y = Freq),
             position = position_stack(vjust = 0.5),
-            colour = I(colour3),
+            colour = I(colour_text),
             size = cex * 6
         ) +
-        scale_y_continuous(breaks = pretty_breaks(n = 3)) # +
-        # scale_x_discrete(
-        #   labels = group_by(df0, var2) %>%
-        #     summarise(label = length(var2)) %>%
-        #     mutate(label = paste0(var2, " (N = ", label, ")")) %>%
-        #     pull(label) %>%
-        #     str_wrap(width_legend)
-        # )
+        scale_y_continuous(breaks = pretty_breaks(n = 3))  +
+        scale_x_discrete(
+          labels = group_by(df0, var2) %>%
+            summarise(label = length(var2)) %>%
+            mutate(
+                label = str_wrap(var2, width_legend) %>%
+                    paste0("\n(N = ", label, ")")) %>%
+                    pull(label)
+
+        )
     if (!is.null(title)) {
         p <- p + labs(title = str_wrap(title, width_title))
     }
@@ -152,8 +153,8 @@ plot_bar_2cat <- function(
     if (stats && nrow(res) > 0) {
         p <- p + stat_pvalue_manual(
             res,
-            label = "fdr.signif",
-            color = colour2,
+            label = "p.signif",
+            color = "gray50",
             bracket.size = 0.7,
             size = cex * 6,
             hide.ns = TRUE,
@@ -167,14 +168,14 @@ plot_bar_2cat <- function(
         cex_sub = cex_sub,
         cex_axis = cex_axis,
         guide = TRUE,
-        color_subtitle = colour2,
+        color_subtitle = "gray50",
         x_axis = TRUE,
         legend = TRUE
     ) +
         theme(
-            axis.text.y = element_text(size = cex * 15, color = colour2),
+            axis.text.y = element_text(size = cex * 15, color = "gray50"),
             axis.text.x = element_text(hjust = 1, vjust = 1),
             legend.title = element_text(face = "bold.italic", size = cex_axis),
-            legend.text = element_text(colour = colour2, size = cex * 15)
+            legend.text = element_text(colour = "gray50", size = cex * 15)
         )
 }
